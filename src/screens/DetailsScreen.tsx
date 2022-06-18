ffbe1eb8963a4e7ff2be0e841ed92b0b0eb02535
Martí Ollé <martiollea@gmail.com>
2022-06-18 23:40:29 +0200

    Nueva versión de diseños.

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -33,15 +33,36 @@ export default function DetailsScreen() {
           <img className="poster" src={comic.thumbnail.path + "." + comic.thumbnail.extension} />
           <div className="text">
             <h1 className="title">{comic.title}</h1>
+            <div className="description">{comic.description}</div>
             <div className="subtitle">
               <div className="year">{getYear(comic.dates[1].date)}</div>
               <div className="space" />
               <div className="price">{comic.prices[0].price}€</div>
               <div className="flex-space" />
             </div>
-            <div className="description">{comic.description}</div>
           </div>
         </div>
+        <div className="characters">
+          <h2>Characters</h2>
+          <div className="characters-list">
+            {comic.characters.items?.map((c) => (
+              <div className="card">
+                <div className="name">{c.name}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="creators">
+        <h2>Creators</h2>
+          <div className="characters-list">
+            {comic.creators.items?.map((c) => (
+              <div className="card">
+                <div className="name">{c.name}</div>
+                <div className="role">{c.role}</div>
+              </div>
+              ))}
+          </div>
+        </div>  
       </div>
     </div>
   );
